db5a52a58c2db63ef48c037f6957bea197b78bd5
Jason.Zhang <lefttjs@gmail.com>
2016-06-20 17:03:25 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export NODE_ENV=production
+git reset --hard
+git pull origin HEAD
+npm install
+pm2 stop qs-manager-server -f
+pm2 start ./server/bin/www -n qs-manager-server
+pm2 stop qs-manager-web -f
+pm2 start ./server.js -n qs-manager-web
