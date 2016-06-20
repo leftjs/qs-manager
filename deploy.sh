18b9f1060b90b92fa04cd6a6395a876a335b4fd3
Jason.Zhang <lefttjs@gmail.com>
2016-06-20 17:27:55 +0800

    dist

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git reset --hard
 git pull origin HEAD
 npm install
 pm2 stop qs-manager-server -f
-pm2 start ./server/bin/www -n qs-manager-server
+pm2 start ./server/bin/www -n qs-manager-server -x babel-node
 pm2 stop qs-manager-web -f
-pm2 start ./server.js -n qs-manager-web
+pm2 start ./server.js -n qs-manager-web -x babel-node
