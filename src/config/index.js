9f691feb7d60f301e10dc98ea7db66ee4c4674ac
Jason.Zhang <lefttjs@gmail.com>
2016-06-20 16:57:29 +0800

    dist

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,5 @@
  * Created by jason on 6/2/16.
  */
 export default {
-	domain: "http://localhost:3030"
+	domain: process.env.NODE_ENV != "production" ? "http://localhost:3030" : "http://qs-admin.lefttjs.com"
 }
