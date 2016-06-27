654d4f684bfbf85d2e48c5991afb4bb26f92352b
Jason.Zhang <lefttjs@gmail.com>
2016-06-27 18:38:34 +0800

    bug fixed

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,5 @@
  * Created by jason on 6/2/16.
  */
 export default {
-	domain: process.env.NODE_ENV != "production" ? "http://localhost:3030" : "http://qs-admin.lefttjs.com"
+	domain: process.env.NODE_ENV != "production" ? "http://localhost:3030" : "http://qs-admin-api.greenicetech.cn"
 }
