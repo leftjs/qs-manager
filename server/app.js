396bfa54648dc2dbc4c7f2569ae3a0b015b81162
Jason.Zhang <lefttjs@gmail.com>
2016-06-28 15:00:30 +0800

    bug fixed

--- server/app.js
+++ server/app.js
@@ -69,11 +69,12 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
+  //res.status(err.status || 500);
+  //res.json({
+  //  message: err.message,
+  //  error: {}
+  //});
+	res.redirect('/')
 });
 
 
